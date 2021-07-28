bed4296315d68f2591013102d54504e1f267f690
JosePasini <josepasini.17@gmail.com>
2021-07-28 19:50:06 -0300

    .

--- src/egg_poo_4/main.java
+++ src/egg_poo_4/main.java
@@ -3,7 +3,6 @@ package egg_poo_4;
 public class main {
     public static void main(String[] args) {
         Rectangulo r = new Rectangulo(15, 15);
-        
         r.dibujarCuadrado();
     }
 }
